9b6bee1e3f29b1c932e2c48afe0c1ae7f77f62fb
teju3805 <tgundupallibtech23@ced.alliance.edu.in>
2026-06-30 19:23:27 +0530

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = {}
+        for ch in s:
+            if ch in count:
+                count[ch] += 1
+            else:
+                count[ch] = 1
+        for ch in t:
+            if ch not in count:
+                return False
+            count[ch] -= 1
+            if count[ch] < 0:
+                return False
+        return True
